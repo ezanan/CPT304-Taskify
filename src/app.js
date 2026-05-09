3110f64c91e4c29de2d13115ccc1f508faa1b2c5
Goodjuices <3260669948@qq.com>
2026-05-09 21:01:30 +0800

    add 404 error page to improve UX

--- src/app.js
+++ src/app.js
@@ -31,6 +31,18 @@ app.get("/dashboard", (req, res) => {
 });
 
 
+app.use((req, res) => {
+    // return 404 
+    res.status(404).send(`
+        <div style="text-align: center; margin-top: 100px; font-family: sans-serif;">
+            <h1 style="font-size: 50px; color: #5a21e6;">404</h1>
+            <h2>Oops! Page Not Found</h2>
+            <p>The page you are looking for does not exist or has been moved.</p>
+            <a href="/" style="display: inline-block; margin-top: 20px; padding: 10px 20px; background-color: #5a21e6; color: white; text-decoration: none; border-radius: 5px;">Go Back Home</a>
+        </div>
+    `);
+});
+
 
 
 //* listen
